bd7e5fd4503c9db8a7e0804f82efbaeb20063942
John O'Connor <john.oconnor@shamrock-space.eu>
2021-02-19 17:05:52 +0000

    Removed some code

--- test.py
+++ test.py
@@ -1,22 +1,9 @@
-import math
-import sys
-from os import rename
-
 import requests
 
 name = input("Your name? ")
 print("Hello, ", name)
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
-
-
 r = requests.get('https://coreyms.com')
 print(r.status_code)
-# print(greet("World"))
-# print(greet('John'))
+print(r.ok)
+
